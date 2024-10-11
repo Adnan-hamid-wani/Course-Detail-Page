383347f988469b83a28700db4f9460e957950099
Adnan-hamid-wani <Adnanaduuu@gmail.com>
2024-10-11 10:44:15 +0530

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "../styles/Header.css";
+import { FolderMinus } from "lucide-react";
+import { Globe } from "lucide-react";
+import GetAppOutlinedIcon from '@mui/icons-material/GetAppOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 export default function Header() {
   return (
@@ -10,40 +14,31 @@ export default function Header() {
             src="https://png.pngtree.com/png-clipart/20211017/original/pngtree-mashallah-3d-golden-calligraphy-arabic-written-text-transparent-background-png-image_6853056.png"
             alt="Logo"
           />
-          <span className="logo-subtext">للبيع والتصميم والتطوير</span>
+          <span className="logo-subtext">الافترايفكتس و البريمي</span>
         </div>
 
         <div className="categories">
-          <img
-            src="https://www.iconbolt.com/iconsets/iconly-light/folder.svg"
-            alt="Categories"
-          />
+        <FolderMinus className="cat__img"/>
+          
           <span>Categories</span>
         </div>
       </div>
 
       <div className="header__center">
-        <img
-          className="search__circle"
-          src="https://www.iconbolt.com/iconsets/anron-line/search.svg"
-        />
+        <SearchOutlinedIcon className="search__circle"/>
         <input type="text" placeholder="Search" />
       </div>
 
       <div className="header__right">
         <button className="app-btn">
-          <img
-            className="app__img"
-            src="https://www.iconbolt.com/iconsets/iconly-bulk/download.svg"
-          />
+        
+        <GetAppOutlinedIcon className="app__img"/>
+
           <p>Get the app</p>
         </button>
 
         <button className="lang-btn">
-          <img
-            className="img_en"
-            src="https://www.iconbolt.com/iconsets/iconoir/language.svg"
-          />
+        <Globe  className=" img_en"/>
           <p>EN</p>
         </button>
 
