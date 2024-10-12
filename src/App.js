3b7fd4e101446a3532ff806057b6425c470d02d8
Adnan-hamid-wani <Adnanaduuu@gmail.com>
2024-10-12 10:57:08 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import "./styles.css";
-import Header from "./components/Header";
+import './App.css';
+import CourseInfoCard from './components/CourseInfoCard';
+import Header from './components/Header';
 
-export default function App() {
-  return (
-    <div className="App">
+function App() {
+  return ( <>
+    <div className="app_header">
       <Header />
     </div>
+
+    <div className='app_body'>
+    
+    </div>
+    <CourseInfoCard
+        title="Intro to UI/UX Design"
+        courseName={'UI/UX Design'}
+        price={75}
+        discountedPrice={40}
+        students={9839}
+        duration={2.5}
+        rating={4.8}
+        reviews={4479}
+        certificate={true}
+      />
+      
+    </>
+    
   );
 }
+
+export default App;
