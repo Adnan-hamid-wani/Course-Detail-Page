fa370fab89acdf98d94e9ac6608409d3407c8c86
Adnan-hamid-wani <Adnanaduuu@gmail.com>
2024-10-14 14:41:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import CourseInfoCard from './components/CourseInfoCard';
 import Header from './components/Header';
+import MentorSection from './components/MentorSection';
 
 function App() {
+
+  
   return ( <>
     <div className="app_header">
       <Header />
     </div>
 
     <div className='app_body'>
-    
-    </div>
     <CourseInfoCard
         title="Intro to UI/UX Design"
         courseName={'UI/UX Design'}
@@ -22,6 +23,22 @@ function App() {
         reviews={4479}
         certificate={true}
       />
+      <MentorSection
+        mentorName={"Adnan wani"}
+        mentorImage={"https://randomuser.me/api/portraits/men/1.jpg"}
+        title={"I am a UI/UX designer with 5+ years of experience"}
+        description={"I have worked with top companies and have a proven track record of delivering high-quality designs.I have worked with top companies and have a proven track record of delivering high-quality designs.I have worked with top companies and have a proven track record of delivering high-quality designs.I have worked with top companies and have a proven track record of delivering high-quality designs.The term  originates from Latin, meaning  or . In the realm of publishing and design, Lorem Ipsum is a commonly used piece of placeholder text, often used to demonstrate the appearance and layout of a webpage, book, or other publication. "}
+        lastUpdated={'Aug 25 2024'}
+        language={'English'}
+        availabilty={"Access on laptops and mobile"}
+        certificate={'Shareable certificate of completion'}
+        access={"Online at your own place"}
+        videoSrc={"https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"} // Pass the YouTube URL as a prop
+
+      />
+    
+    </div>
+    
       
     </>
     
